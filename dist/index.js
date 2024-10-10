6032b16bd8b07a55e799fb6deab2298a5b21ccfc
DavideViolante <fungtq3@gmail.com>
2024-10-10 12:34:51 +0200

    change gitignore to include dist index file

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello, world!');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const b = 2;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const c = 3;
+console.log(c);
