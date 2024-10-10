b74582f910cbe83d53738f15b3ff37d0f2a73c06
DavideViolante <fungtq3@gmail.com>
2024-10-10 12:38:10 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,5 @@
 console.log('Hello, world!');
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const b = 2;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const c = 3;
 console.log(c);
